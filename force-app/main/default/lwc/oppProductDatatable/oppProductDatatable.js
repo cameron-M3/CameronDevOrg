53ce2aa6e68273d8ceb29abe0d05740a518f8905
cameron <jones.cameroncanon@gmail.com>
2024-06-06 12:16:04 -0600

    changed code

--- force-app/main/default/lwc/oppProductDatatable/oppProductDatatable.js
+++ force-app/main/default/lwc/oppProductDatatable/oppProductDatatable.js
@@ -1,25 +1,25 @@
 import { LightningElement, api, wire, track } from 'lwc';
-import { updateRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { refreshApex } from '@salesforce/apex';
 import getOpportunityProducts from '@salesforce/apex/OpportunityProductController.getOpportunityProducts';
+import updateOpportunityProducts from '@salesforce/apex/OpportunityProductController.updateOpportunityProducts';
 import NAME_FIELD from '@salesforce/schema/OpportunityLineItem.Name';
 import QUANTITY_FIELD from '@salesforce/schema/OpportunityLineItem.Quantity';
 import UNITPRICE_FIELD from '@salesforce/schema/OpportunityLineItem.UnitPrice';
 import SERVICEDATE_FIELD from '@salesforce/schema/OpportunityLineItem.ServiceDate';
+import getTotalAmount from '@salesforce/apex/OpportunityProductController.getTotalAmount';
 
 
 export default class OppProductDatatable extends LightningElement {
 
-    @api recordId; 
+    @api recordId;
 
-    results;
-    records = [];
-    tableDraftValues =[];
+    draftValues = [];
+    totalAmount = 0;
 
 
     //create columns for datatable
-     @track columns = [
+     columns = [
         { label: 'Name', fieldName: NAME_FIELD.fieldApiName, type: 'text', editable: true },
         { label: 'Quantity', fieldName: QUANTITY_FIELD.fieldApiName, type: 'number', editable: true },
         { label: 'Sales Price', fieldName: UNITPRICE_FIELD.fieldApiName, type: 'currency', editable: true },
@@ -28,57 +28,43 @@ export default class OppProductDatatable extends LightningElement {
 
 
     @wire(getOpportunityProducts, {oppId: '$recordId'})
-    wiredOppProducts(opr){
-        this.results = opr;
+    oppProducts;
 
-        if(this.results.data){
-            console.log('data returned');
-            this.records = this.results.data;
-        }
-        if(this.results.errors){
-            console.log('error getting records')
-        }
-    }
+    @wire(getTotalAmount, {oppId: '$recordId'})
+    
 
-    tableSave(event){
-        this.tableDraftValues = event.detail.draftValues;
+    
 
-        const inputItems = this.tableDraftValues.slice().map(draft =>{
-            var fields = Object.assign({}, draft);
-            return { fields };
-        })
-        const promises = inputItems.map(recordInput => updateRecord(recordInput));
-        Promise.all(promises)
-        .then(result => {
+    async handleSave(event){
+        const updatedFields = event.detail.draftValues;
+
+        this.draftValues = [];
+
+        try {
+            //pass edited fields to the updateOpportunity Apex Controller
+            await updateOpportunityProducts({oppProductsForUpdate: updatedFields});
+
+
+            //Report success with a Toast
             this.dispatchEvent(
                 new ShowToastEvent({
-                    title: 'Success!',
-                    message: 'Successfully updated the records.',
-                    variant: 'success',
-                    mode: 'dismissible'
+                    title: 'Opportunity Products Saved Successfully',
+                    message: 'The opportunity products records was saved successfully',
+                    variant: 'success'
                 })
             );
-            refreshApex(this.records);
-        })
-        .catch(error => {
+
+            //Display fresh data in datatable
+            await refreshApex(this.oppProducts);
+
+        } catch (error){
             this.dispatchEvent(
                 new ShowToastEvent({
-                    title: 'Error!',
-                    message: 'Error occurred updating records.',
-                    variant: 'error',
-                    mode: 'dismissible'
+                    title: 'Error while updating or refreshing records',
+                    message: error.body.message,
+                    variant: 'error'
                 })
             );
         }
-        )
-        .finally(result => {
-            this.myDrafts = [];
-            console.log('Finally');
-            refreshApex(this.records);
-        });
-
-
-
     }
-
 }
